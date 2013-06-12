50ca8ec5193055fdda6b655e73239175f177bdfa
Victor Safronovich <vsafronovich@gmail.com>
2013-06-12 18:35:10 +0600

    added license file update installer

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -
 #
 # This file is part of django-privatesite released under the MIT license. 
-# See the NOTICE for more information.
+# See the LICENSE for more information.
 
-import os
-import sys
 from setuptools import setup, find_packages
-
-from privatesite import VERSION
 
 setup(
     name='django-privatesite',
-    version=VERSION,
+    version=__import__('privatesite').VERSION,
     description='Django custom admins',
-    long_description=file(
-        os.path.join(
-            os.path.dirname(__file__),
-            'README.md'
-        )
-    ).read(),
+    long_description=open('README.md').read(),
     author='Victor Safronovich',
     author_email='vsafronovich@gmail.com',
     license='MIT',
@@ -29,4 +20,16 @@ setup(
                       'django-apptemplates>=0.0.1',
                      ],
     include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Web Environment',
+        'Topic :: Software Development',
+    ]
 )
